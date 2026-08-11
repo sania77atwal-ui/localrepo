5068d934dfc90c12ec22d8326a249946027120ea
sania77atwal-ui <sania77atwal@gmail.com>
2026-08-11 21:23:56 +0530

    corrected code

--- Function.java
+++ Function.java
@@ -1,12 +1,26 @@
 import java.util.Scanner;
 
 public class Function {
-    public static int fibonacaSeries(int x){
+    public static int fibonacaSeries(int x) {
+        int fn, fn1, fn2;
+        
+        fn1 = 1;
+        fn2 = 0;
+        System.out.println(fn2);
+        System.out.println(fn1);
+        for (int i = 1; i <= x; i++) {
+            fn = fn1 + fn2;
+            fn2=fn1;
+            fn1=fn;
+           System.out.println(fn);
+        }
+
         return 0;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("enter the first number:");
+        System.out.print("enter the limit:");
         int a = sc.nextInt();
         fibonacaSeries(a);
     }
